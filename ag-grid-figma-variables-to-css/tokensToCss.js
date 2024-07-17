fa1dd152809d7df6aa4aadc4777b97c8c6d3b6e3
Tak Tran <contact@tutaktran.com>
2024-07-17 10:20:43 +0100

    Fix up tokens to css script
    Fix up references before passing into StyleDictionary, and have a pass for light and dark mode

--- ag-grid-figma-variables-to-css/tokensToCss.js
+++ ag-grid-figma-variables-to-css/tokensToCss.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const StyleDictionary = require("style-dictionary");
 const allProps = require("./data/all-props.json");
 
-
 // Specify path for Figma "Variables Import Export" plugin json output
 let tokenPath = process.argv[2];
 
@@ -16,88 +15,126 @@ if (!tokenPath) {
 // Read and parse json input
 const tokenInput = JSON.parse(fs.readFileSync(tokenPath, "utf-8"));
 
-// Clone the tokens to be able to compare
-const trimmedTokens = structuredClone(tokenInput);
-
-// Remove unnecessary collections so we don't trigger reference errors 
-delete trimmedTokens.densities;
-delete trimmedTokens.admin;
-delete trimmedTokens.charts;
-if (trimmedTokens.themes.mode) {
-  delete trimmedTokens.themes.mode.comp;
-}
-
-// Get rid of weird specific problems
-delete trimmedTokens.themes.alpine.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.mode.light.theme.alpine.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.mode.light.theme.quartz.comp['ag-border-grid-container'];
-delete trimmedTokens.mode.light.theme.quartz.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.mode.light.theme.material.comp['ag-side-button-selected-background-color'];
-delete trimmedTokens.mode.light.theme.material.comp['ag-toggle']['active-background'];
-delete trimmedTokens.mode.light.theme.material.comp['ag-control']['active-background'];
-delete trimmedTokens.mode.light.theme.material.comp['ag-border-grid-container'];
-delete trimmedTokens.mode.light.theme.material.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.mode.dark.theme.alpine.comp['ag-border-grid-container'];
-delete trimmedTokens.mode.dark.theme.quartz.comp['ag-border-grid-container'];
-delete trimmedTokens.mode.dark.theme.quartz.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.mode.dark.theme.material.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.themes.material.comp['ag-side-button-selected-background-color'];
-delete trimmedTokens.themes.material.comp['ag-border-grid-container'];
-delete trimmedTokens.themes.material.comp['ag-side-button-panel-background-color'];
-delete trimmedTokens.themes.quartz.comp['ag-border-grid-container'];
-delete trimmedTokens.themes.quartz.comp['ag-side-button-panel-background-color'];
-
-// Extract theme names from json
-const themeNames = Object.keys(trimmedTokens.themes).filter((themeName) => themeName !== 'mode');
-
-// Remove any grouped properties 
-for (const themeName of themeNames) {
-  const comp = trimmedTokens.themes[themeName].comp;
-
-  for (const key in comp) {
-    if (comp[key].type === undefined) {
-      delete comp[key]
-    }
-  }
-}
-
 // CSS output filename
 const outputFile = "./css/new-ag-grid-themes.css";
 
+function getModeThemeNames(tokens, mode) {
+  return Object.keys(tokens.mode[mode].theme).filter(
+    (themeName) => themeName !== "mode"
+  );
+}
+
+function getReferencePath(value) {
+  if (!value || typeof value !== "string") {
+    return false;
+  }
+
+  const [, match] = value.match(/^\{(.*)\}/) || [];
+  return match;
+}
+
+function updateThemeComp({ comp, mode }) {
+  Object.entries(comp).forEach(([, propertyObj]) => {
+    const isNested = propertyObj.value === undefined;
+
+    if (isNested) {
+      updateThemeComp({ comp: propertyObj, mode });
+    } else {
+      const refPath = getReferencePath(propertyObj.value);
+      if (refPath) {
+        const path = refPath.split(".");
+        if (path[0] === "mode") {
+          const newPath = [path[0], mode, ...path.slice(2)];
+          propertyObj.value = `{${newPath.join(".")}}`;
+        }
+      }
+    }
+  });
+}
+
+function updateModeComp({ comp, themeName }) {
+  Object.entries(comp).forEach(([, propertyObj]) => {
+    const isNested = propertyObj.value === undefined;
+
+    if (isNested) {
+      updateModeComp({ comp: propertyObj, themeName });
+    } else {
+      const refPath = getReferencePath(propertyObj.value);
+      if (refPath) {
+        const path = refPath.split(".");
+        if (path[0] === "themes") {
+          const newPath = [path[0], themeName, ...path.slice(2)];
+          propertyObj.value = `{${newPath.join(".")}}`;
+        }
+      }
+    }
+  });
+}
+
+/**
+ * Update token references for StyleDictionary
+ *
+ * NOTE: Modifies `tokens`
+ */
+function updateTokenReferences({ tokens, mode }) {
+  // Update theme comp references
+  // Eg, In `themes.material.comp.ag-active-color.value`
+  // Convert `mode.material.theme.material.comp.ag-active-color` to `mode.light.theme.material.comp.ag-active-color`
+  Object.entries(tokens.themes).forEach(([, themeObj]) => {
+    updateThemeComp({ comp: themeObj.comp, mode });
+  });
+
+  // Update mode theme comp references
+  // Eg, In `mode.light.theme.material.comp.ag-toggle.active-background.value`
+  // Convert `themes.light.comp.ag-checkbox-checked-color` -> `themes.material.comp.ag-checkbox-checked-color`
+  Object.entries(tokens.mode[mode].theme).forEach(([themeName, themeObj]) => {
+    updateModeComp({ comp: themeObj.comp, themeName });
+  });
+
+  // Delete other mode, so all references are valid
+  const otherModes = Object.keys(tokens.mode).filter((m) => m !== mode);
+  for (const otherMode of otherModes) {
+    delete tokens.mode[otherMode];
+  }
+}
+
 // Transform and save an AG grid theme css file for each Figma variable collection
-const outputTheme = (themeTokens) => {
+const outputTheme = (tokenContents) => {
   let cssOutput = "";
 
-  // const themeNames = Object.keys(themeTokens.themes);
+  ["light", "dark"].forEach((mode) => {
+    // Clone the tokens to be able to compare
+    const tokens = structuredClone(tokenContents);
+    const themeNames = getModeThemeNames(tokens, mode);
+    updateTokenReferences({ tokens, mode });
 
-  // Style dictionary css variables config
-  const dictionaryConfig = {
-    tokens: themeTokens,
-    platforms: {
-      css: {
-        transformGroup: "css",
-        options: {
-          outputReferences: true,
+    // Style dictionary css variables config
+    const dictionaryConfig = {
+      tokens,
+      platforms: {
+        css: {
+          transformGroup: "css",
+          options: {
+            outputReferences: true,
+          },
         },
       },
-    },
-  };
+    };
 
-  const tokenExport =
-    StyleDictionary.extend(dictionaryConfig).exportPlatform("css");
+    const tokenExport =
+      StyleDictionary.extend(dictionaryConfig).exportPlatform("css");
 
-  ["light", "dark"].forEach((mode) => {
     themeNames.forEach((themeName) => {
-      const tokens = tokenExport.mode[mode].theme[themeName].comp;
+      const themeTokens = tokenExport.mode[mode].theme[themeName].comp;
 
       const selector = `.ag-theme-${themeName}-${mode} {\n`;
 
       cssOutput += selector;
 
-      Object.keys(tokens).forEach((key) => {
+      Object.keys(themeTokens).forEach((key) => {
         if (!allProps.includes(key)) return;
 
-        const value = tokens[key].value;
+        const value = themeTokens[key].value;
 
         cssOutput += `  --${key}: ${value};\n`;
       });
@@ -111,4 +148,4 @@ const outputTheme = (themeTokens) => {
   console.log(`✔︎ ${outputFile} saved!`);
 };
 
-outputTheme(trimmedTokens);
+outputTheme(tokenInput);
