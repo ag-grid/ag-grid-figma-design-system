423f9e22c0bf3c1a893141329f3901b91cbc9ccc
Mark Durrant <mark.j.durrant@gmail.com>
2025-07-16 11:58:53 +0100

    Start on Figma tokens to JS theme example project

---
+++ ag-grid-figma-local-variables-to-theme/tokens-to-ag-grid-theme.js
@@ -0,0 +1,52 @@
+import StyleDictionary from "style-dictionary";
+import {
+  convertTokenData,
+  resolveReferences,
+  usesReferences,
+} from "style-dictionary/utils";
+
+import exampleTokens from "./tokens/example-tokens-14-07-25.json" with { type: "json" };
+
+const THEME = "quartz";
+const MODE = "light";
+
+const sd = new StyleDictionary({
+  tokens: exampleTokens,
+});
+
+await sd.hasInitialized;
+
+const themeTokensData = convertTokenData(sd.tokens, { output: "object" });
+
+// For color token references replace first occurrence of THEME with MODE
+// This fixes complexity in our tokens output caused by Figma theme switcher
+const transformColorValue = (colorValue) => {
+  if (typeof colorValue !== "string") return colorValue;
+
+  return colorValue.replace(THEME, MODE);
+};
+
+const transformColorTokens = (colorTokens) => {
+  return Object.entries(colorTokens).reduce((acc, [key, color]) => {
+    acc[key] = { ...color, value: transformColorValue(color.value) };
+
+    return acc;
+  }, {});
+};
+
+const themeSizeTokensObj = themeTokensData["ag-theme"][THEME].size;
+const themeColorTokensObj = transformColorTokens(
+  themeTokensData["ag-theme"][THEME].color
+);
+
+const themeTokensObj = { ...themeSizeTokensObj, ...themeColorTokensObj };
+
+const theme = Object.entries(themeTokensObj).reduce((acc, [key, token]) => {
+  acc[key] = usesReferences(token.value)
+    ? resolveReferences(token.value, sd.tokens, { warnImmediately: false })
+    : token.value;
+
+  return acc;
+}, {});
+
+console.log(theme);
